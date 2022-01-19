e900073fed470859c258b5f1ea7029419a7ff378
Abolade Greatness <49532326+Grtnxhor@users.noreply.github.com>
2022-01-19 23:39:26 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+$token = '5040140768:AAETyGZKm6yKSWNsgGeCHF7zhVBY9vLeyMY';
+$input = file_get_contents("https://api.telegram.org/bot$token/getUpdates");
+$output = json_decode($input);
+
+print_r($output->new_chat_members);
