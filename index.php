0765c9a597ff23f7d243e6e0f7aa477c298e371f
Abolade Greatness <49532326+Grtnxhor@users.noreply.github.com>
2022-01-19 23:56:26 +0100

    on ti

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
+$input = file_get_contents('php://input');
 $token = '5040140768:AAETyGZKm6yKSWNsgGeCHF7zhVBY9vLeyMY';
 $input = file_get_contents("https://api.telegram.org/bot$token/getUpdates");
 $output = json_decode($input);
 
-print_r($output->new_chat_members);
+print_r($output->result[0]);
