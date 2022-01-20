2ef1df509fa1374076cda2f6006fc875eb65081e
Abolade Greatness <49532326+Grtnxhor@users.noreply.github.com>
2022-01-20 01:18:14 +0100

    fixed

--- index.php
+++ index.php
@@ -3,5 +3,10 @@ $input = file_get_contents('php://input');
 $token = '5040140768:AAETyGZKm6yKSWNsgGeCHF7zhVBY9vLeyMY';
 $input = file_get_contents("https://api.telegram.org/bot$token/getUpdates");
 $output = json_decode($input);
+$update_id = $output->result[0]->update_id;
 
-print_r($output->result[0]);
+if(isset($output->result[0]->message->new_chat_member)) {
+
+//tell us who the person is
+print_r($output->result[0]->message->new_chat_member);
+}
