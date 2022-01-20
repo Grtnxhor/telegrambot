ed38a9860ccbc4997439c1253d2ad45cdeebbfbe
Abolade Greatness <49532326+Grtnxhor@users.noreply.github.com>
2022-01-20 04:56:46 +0100

    bot

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
-
 $input = file_get_contents('php://input');
 $token = '5040140768:AAETyGZKm6yKSWNsgGeCHF7zhVBY9vLeyMY';
 $input = file_get_contents("https://api.telegram.org/bot$token/getUpdates");
 $output = json_decode($input);
-//$updatid = $output->result[0]->update_id;
+$updatid = $output->result[0]->update_id;
 
 
 //check if there is a new user
@@ -17,5 +16,5 @@ $username = $output->result[0]->message->new_chat_member->username;
 $send = file_get_contents("https://api.telegram.org/bot$token/sendmessage?chat_id=$new_user_id&text=hello $username Welcome here");
 
 //offset id
-//$offset = file_get_contents("https://api.telegram.org/bot$token/getUpdates?offset=$updatid");
+$offset = file_get_contents("https://api.telegram.org/bot$token/getUpdates?offset=$updatid");
 }
--- test.php
+++ test.php
@@ -1,5 +1,3 @@
-echo '
-
 <script>
 setInterval(function() {
     '.telebot().'
